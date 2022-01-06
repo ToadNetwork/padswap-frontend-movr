23a1902a3fe8fd8d24c26eda610734fd8403e23b
toadguy <contact@toad.network>
2022-01-06 18:55:15 -0300

    updated UI kit

--- @padswap/sdk/dist/constants.d.ts
+++ @padswap/sdk/dist/constants.d.ts
@@ -1,8 +1,8 @@
 import JSBI from 'jsbi';
 export declare type BigintIsh = JSBI | bigint | string;
 export declare enum ChainId {
-    MAINNET = 56,
-    BSCTESTNET = 97
+    MAINNET = 1285,
+    TESTNET = 97
 }
 export declare enum TradeType {
     EXACT_INPUT = 0,
@@ -13,8 +13,8 @@ export declare enum Rounding {
     ROUND_HALF_UP = 1,
     ROUND_UP = 2
 }
-export declare const FACTORY_ADDRESS = "0xB836017ACf10b8A7c6c6C9e99eFE0f5B0250FC45";
-export declare const INIT_CODE_HASH = "0x71e6e31358ee16b3d9300fae77629fc40a171f4cc53183576ba7ba5fb318c931";
+export declare const FACTORY_ADDRESS = "0x760d2Bdb232027aB3b1594405077F9a1b91C04c1";
+export declare const INIT_CODE_HASH = "0xc5443539aede6901cfc8cbd4f358bb6c6524f2d1bd7ecd1f6ebea4d34498726d";
 export declare const MINIMUM_LIQUIDITY: JSBI;
 export declare const ZERO: JSBI;
 export declare const ONE: JSBI;
@@ -23,8 +23,8 @@ export declare const THREE: JSBI;
 export declare const FIVE: JSBI;
 export declare const TEN: JSBI;
 export declare const _100: JSBI;
-export declare const _997: JSBI;
-export declare const _1000: JSBI;
+export declare const FEES_NUMERATOR: JSBI;
+export declare const FEES_DENOMINATOR: JSBI;
 export declare enum SolidityType {
     uint8 = "uint8",
     uint256 = "uint256"
